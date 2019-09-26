23108e49c085291259ab223a2a38debc929e1c5f
Maddie <maddiemcampbell@gmail.com>
2019-09-26 11:35:58 -0700

    populated list with top spots

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import TopSpot from './topspot';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        topspots: []
+    }
+}
+
+componentWillMount() {
+  axios
+  .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+  .then(response => response.data)
+  .then(topspots => this.setState({ topspots }));
+}
   render() {
     return (
-      <div className='App'></div>
+      <body>
+        <div className='app'>
+          <div className='contentWrapper'>
+            <h1>
+              San Diego Top Spots
+            </h1>
+            <h3>
+              A list of the top 30 places to see in San Diego, California.
+            </h3>
+          </div>
+          <div className='contentWrapper'>
+            {
+            this.state.topspots.map(topspot => (
+              <TopSpot
+                key={ topspot.id }
+                name={ topspot.name }
+                description={ topspot.description }
+                location={ topspot.location } 
+            />
+            ))
+        }
+          </div>
+          
+        </div>
+      </body>
     );
   }
 }
---
+++ src/topspot.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props => (
+    <div className='well'>
+        <h4>{props.name}</h4>
+        <p>{props.description}</p>
+    </div>
+);
