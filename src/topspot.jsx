269bbae6111323c12b6454bb9f62b08fbf1377f3
Maddie <maddiemcampbell@gmail.com>
2019-09-26 12:06:06 -0700

    styling for location button

--- src/topspot.jsx
+++ src/topspot.jsx
@@ -4,8 +4,8 @@ export default props => (
     <div className='well'>
         <h4>{props.name}</h4>
         <p>{props.description}</p>
-        <button type="button" className="btn btn-info">
-        <a href={"https://www.google.com/maps?q=" + props.location}>Link to Google Maps</a>
-    </button>
+        <button type="button" className="btn btn-info" id="button">
+            <a href={"https://www.google.com/maps?q=" + props.location}>Link to Google Maps</a>
+        </button>
     </div>
 );
